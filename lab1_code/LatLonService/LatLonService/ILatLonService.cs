0cfd78d9f2ae529b60143d454866a383cadcb535
Kone Fanhatcha <fanhatcha@projanalytics.com>
2020-02-17 11:46:00 +0400

    Lab 1

---
+++ lab1_code/LatLonService/LatLonService/ILatLonService.cs
@@ -0,0 +1,28 @@
+﻿// original code by Bret Stateham - MS "Developing Windows Azure and Web Services Jump Start"
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace LatLonService
+{
+    [ServiceContract(Namespace = "http://gcu.ac.uk/cpd/2017/06/LatLon")]
+    public interface ILatLonService
+    {
+        [OperationContract]
+        double RadiansBetweenTwoPoints(double Latitude1, double Longitude1, double Latitude2, double Longitude2);
+
+        [OperationContract]
+        double NauticalMilesBetweenTwoPoints(double Latitude1, double Longitude1, double Latitude2, double Longitude2);
+
+        [OperationContract]
+        double KilometersBetweenTwoPoints(double Latitude1, double Longitude1, double Latitude2, double Longitude2);
+
+        [OperationContract]
+        double MilesBetweenTwoPoints(double Latitude1, double Longitude1, double Latitude2, double Longitude2);
+    }
+}
